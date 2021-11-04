cc028b95f722e27774d1ab5bab0156fa1f18bb86
David Sena <dsenaldam1@gmail.com>
2021-11-04 19:52:23 +0100

    Terminada clase TestEnvio

---
+++ TestEnvio.java
@@ -0,0 +1,29 @@
+
+/**
+ *  
+ * 
+ * @author  David Sena
+ * @version  
+ */
+public class TestEnvio
+{
+     
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+         Paquete paquete1 = new Paquete();
+         Paquete paquete2 = new Paquete(40, 60, 40);
+         Envio envio = new Envio();
+         envio.addPaquete(paquete1);
+         envio.addPaquete(paquete2);
+         //envio.print();
+         Paquete paquete3 = new Paquete(new Dimension(50,50,70), 13);
+         envio.addPaquete(paquete3);
+         envio.print();
+         Paquete copiaPaquete2 = paquete2.obtenerCopia();
+         envio.addPaquete(copiaPaquete2);
+    }
+    
+}
